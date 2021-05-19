650300115cf016fd240cf85e6df9ab0111cb9577
Ykrop3000 <grisha.andreev.03@gmail.com>
2021-05-19 21:06:12 +0300

    add metrics

--- pages/_document.js
+++ pages/_document.js
@@ -38,6 +38,40 @@ export default class MyDocument extends Document {
             name="description"
             content="Смотреть аниме онлайн бесплатно. Большая база лучших аниме с русской озвучкой в хорошем качестве."
           />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-GHJMS3JFVJ"
+          ></script>
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+              
+                gtag('config', 'G-GHJMS3JFVJ');
+              `,
+            }}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+          
+                 (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                 m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                 (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+              
+                 ym(74589367, "init", {
+                      clickmap:true,
+                      trackLinks:true,
+                      accurateTrackBounce:true,
+                      webvisor:true
+                 });
+             
+             `,
+            }}
+          />
         </Head>
         <body>
           <Main />
