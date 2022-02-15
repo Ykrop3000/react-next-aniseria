58cc6cc049b751ec612dadacf061ed702ee037d4
Ykrop3000 <grisha.andreev.03@gmail.com>
2022-02-16 00:26:40 +0300

    delete analitics

--- pages/_document.js
+++ pages/_document.js
@@ -5,74 +5,34 @@ import ServerStyleSheets from '@mui/styles/ServerStyleSheets';
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang="ru">
+      <Html lang='ru'>
         <Head>
           <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width"
+            name='viewport'
+            content='minimum-scale=1, initial-scale=1, width=device-width'
           />
           <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png"
+            rel='apple-touch-icon'
+            sizes='180x180'
+            href='/apple-touch-icon.png'
           />
           <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
+            rel='icon'
+            type='image/png'
+            sizes='32x32'
+            href='/favicon-32x32.png'
           />
           <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
+            rel='icon'
+            type='image/png'
+            sizes='16x16'
+            href='/favicon-16x16.png'
           />
-          <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <link rel='manifest' href='/site.webmanifest' />
+          <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
 
-          <meta name="msapplication-TileColor" content="#da532c" />
-          <meta name="theme-color" content="#ffffff" />
-
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-GHJMS3JFVJ"
-          ></script>
-
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-              
-                gtag('config', 'G-GHJMS3JFVJ');
-              `,
-            }}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-          
-                 (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                 m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-                 (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-              
-                 ym(74589367, "init", {
-                      clickmap:true,
-                      trackLinks:true,
-                      accurateTrackBounce:true,
-                      webvisor:true
-                 });
-             
-             `,
-            }}
-          />
-          <script
-            data-ad-client="ca-pub-5730249981635951"
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-          ></script>
+          <meta name='msapplication-TileColor' content='#da532c' />
+          <meta name='theme-color' content='#ffffff' />
         </Head>
         <body>
           <Main />
